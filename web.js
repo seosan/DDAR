d613edda0788e6fb37b73998b51a97901b7b8a6a
seosan <lithrome@gmail.com>
2014-03-27 15:56:59 +0900

    a

--- web.js
+++ web.js
@@ -7,10 +7,10 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
 });
-/*app.get('/cc.js', function(req, res) {
-  res.sendfile('/usr/cc.js', {root:__dirname});
+app.get('/paper.js', function(req, res) {
+  res.sendfile('/usr/paper-core.min.js', {root:__dirname});
 });
-*/var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
