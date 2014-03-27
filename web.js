4a2fe608e6a42d4b2cf8b11e3bc1ebe60e2ee9c3
seosan <lithrome@gmail.com>
2014-03-27 15:26:49 +0900

    A

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-var cnt = 0;
 app.use(logfmt.requestLogger());
 
 
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
-  console.log(++cnt);
 });
-app.get('/js.js', function(req, res) {
-  res.sendfile('/usr/js.js', {root:__dirname});
+app.get('/cc.js', function(req, res) {
+  res.sendfile('/usr/cc.js', {root:__dirname});
 });
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
