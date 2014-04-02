407438511604a725d067fbc7af30cecdd399d56e
seosan <lithrome@gmail.com>
2014-04-02 09:45:18 +0900

    MAC

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ app.get('/usr/style.css', function(req, res) {
   res.sendfile('/usr/style.css', {root:__dirname});
 });
 app.get('/usr/buggedbit.ttf', function(req, res) {
-  res.sendfile('/usr/buggedbit.ttf', {root:__dirname});
+  res.sendfile('/usr/BuggedBit.ttf', {root:__dirname});
 });
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
